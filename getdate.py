ac5da463fabf011ec18e1b622fa8cdfd7dd6f2e7
Gon <gon@Gonui-Macmini.local>
2022-02-11 11:30:55 +0900

    특정 요일의 날짜+한글요일 반환 스크립트

---
+++ getdate.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+import sys
+
+_str_weekday = ['월', '화', '수', '목', '금', '토', '일']
+
+week_str = sys.argv[1]
+
+date_now = datetime.now()
+date_result = datetime.now()
+date_ref_monday = date_now - timedelta(days = date_now.weekday())
+
+if week_str == "mon":
+	date_result = date_ref_monday + timedelta(days=0)
+elif week_str == "tue":
+	date_result = date_ref_monday + timedelta(days=1)
+elif week_str == "wed":
+	date_result = date_ref_monday + timedelta(days=2)
+elif week_str == "thu":
+	date_result = date_ref_monday + timedelta(days=3)
+elif week_str == "fri":
+	date_result = date_ref_monday + timedelta(days=4)
+elif week_str == "sat":
+	date_result = date_ref_monday + timedelta(days=5)
+elif week_str == "sun":
+	date_result = date_ref_monday + timedelta(days=6)
+
+print("(" + date_result.strftime("%m/%d") + "," + _str_weekday[date_result.weekday()] + ")")
